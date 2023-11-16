7cced6d96e0868c6dd3fe37fe8c90c88298a7e6a
Eric093 <eric.blot1@gmail.com>
2023-11-16 16:02:23 +0100

    ajoiut log et changement de page

--- v1/chapter2/MY_selectByClass_2.py
+++ v1/chapter2/MY_selectByClass_2.py
@@ -1,8 +1,15 @@
 # Version avec gestion des deux erreurs: url + fichier
 # Renvoie toujours none quelle que soit l'erreur
 
+import logging
+
 from urllib.error import HTTPError
 from urllib.error import URLError
+
+# Paramétrage des logs -------------------------
+from logging.handlers import RotatingFileHandler
+logging.basicConfig(filename='log/programlog.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s -  %(message)s')
+# ----------------------------------------------
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -22,20 +29,20 @@ def getTitle(url):
 
     # Scrapper
     try:
-        html = urlopen("https://www.conanauction.fr/")  # A retoucher (getTitle url présent plus bas)
+        html = urlopen("https://www.conanauction.fr/resultats-des-ventes?year=2021&tab=tab2#tj_container_nbre")  # A retoucher (getTitle url présent plus bas)
         bsObj = BeautifulSoup(html, "html.parser")
 
         ##### Test pour n'avoir que le calendrier
         # nameList = bsObj.findAll("div", {"class":"entry-title"})
-        # nameList = bsObj.find("div", {"class":"page_calendrier"}).findAll("div", {"class":"entry-title"})
-        nameList = bsObj.find("div", {"class":"page_calendrier"}).findAll("div", {"class":"entry-title"}).get("href")
+        nameList = bsObj.find("div", {"class":"page_calendrier"}).findAll("div", {"class":"entry-title"})
+        #nameList = bsObj.find("div", {"class":"page_calendrier"}).findAll("div", {"class":"entry-title"}).get("href")
         ######## Avec get href ne renvoie rien ? ajouter un find a ?
 
 
         for name in nameList:
             print(name.get_text())
-            #if 'href' in name.attrs :
-            print(name.get_url())
+            if 'href' in name.attrs :
+                print(name.get_url())
             
             
     except AttributeError as e:
